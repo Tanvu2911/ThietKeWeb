3234003219022eab50714dd899a8bfb7c8dc3468
Tanvu2911 <vuviettan29112005@gmail.com>
2025-09-03 22:51:01 +0700

    Add project files.

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product{Id=1, Name="N?i cõm ði?n cao t?n Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=8500000},
+            new Product{Id=2, Name="N?i cõm ði?n cao t?n Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=8500000},
+            new Product{Id=3, Name="N?i cõm ði?n cao t?n Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=8500000}
+        };
+
+        // Hi?n th? t?t c? s?n ph?m
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // Partial View: hi?n th? menu danh m?c
+        public IActionResult CategoryMenu()
+        {
+            var categories = new List<string> { "Áo dài", "Áo ðông", "Túi xách", "Ð?ng h?", "Ví da", "Thát lýng da", "T? l?nh", "Tivi", "Qu?t ði?n", "L? sý?i" };
+            return PartialView("_CategoryMenu", categories);
+        }
+    }
+}
---
+++ Lab3/ViewComponents/NewProductsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Lab03.ViewComponents
+{
+    public class NewProductsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var newProducts = new List<Product>
+            {
+                new Product{Id=10, Name="Nồi cơm điện cao tần Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=1200000},
+                new Product{Id=11, Name="Nồi cơm điện cao tần Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=8500000},
+                new Product{Id=12, Name="Nồi cơm điện cao tần Nagakawa NAG0102", Image="/images/Noicom.jpg", Price=8500000}
+            };
+            return View(newProducts);
+        }
+    }
+}
